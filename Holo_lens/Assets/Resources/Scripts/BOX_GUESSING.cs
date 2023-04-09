d74fade44e5b639b6a15416a2560f514ddfaccf0
dhtnekfzz <asd7726191@naver.com>
2023-04-09 14:53:47 +0900

    Questioning HashSet

--- Holo_lens/Assets/Resources/Scripts/BOX_GUESSING.cs
+++ Holo_lens/Assets/Resources/Scripts/BOX_GUESSING.cs
@@ -27,25 +27,23 @@ public class BOX_GUESSING : MonoBehaviour
 
     private void Questioning()
     {
+        Qnum = Qs[Qnow].GetComponentsInChildren<Transform>().Length - 1;
+        int s=UnityEngine.Random.Range(0, Buttons.Length);
         HashSet<int> uniqueValues = new HashSet<int>();
-        int s = UnityEngine.Random.Range(0, Buttons.Length);
-        Qnum = findQ().GetComponentsInChildren<Transform>().Length - 1;
         uniqueValues.Add(Qnum);
         while (uniqueValues.Count < Buttons.Length)
         {
-            int r = UnityEngine.Random.Range(Qnum - 3, Qnum + 3);
-           
+            int r=UnityEngine.Random.Range(Qnum-3, Qnum+3);
             uniqueValues.Add(r);
-            
         }
-        int[] Anums = uniqueValues.ToArray();
+        Anums= uniqueValues.ToArray();
 
         for (int i = 0; i < Anums.Length; i++)
         {
-            int randomIndex = UnityEngine.Random.Range(0, Anums.Length);
             int temp = Anums[i];
-            Anums[i] = Anums[randomIndex];
-            Anums[randomIndex] = temp;
+            int r = UnityEngine.Random.Range(0, Anums.Length);
+            Anums[i] = Anums[r];
+            Anums[r] = temp;
         }
 
         for (int i = 0; i < Anums.Length; i++)
@@ -55,14 +53,14 @@ public class BOX_GUESSING : MonoBehaviour
                 AButton = Buttons[i];
         }
 
-        Debug.Log($"{Anums[0]}" + $"{Anums[1]}" + $"{Anums[2]}" + $"{Anums[3]}" + $"{Anums[4]}");
-
-    } 
+    }
 
     public void HightlightAButton()
     {
         AButton.transform.GetChild(1).GetChild(0).GetComponent<MeshOutline>().enabled = true;
     }
+
+   
 
     public void BindButton()
     {
@@ -71,33 +69,32 @@ public class BOX_GUESSING : MonoBehaviour
             go.GetComponent<Interactable>().OnClick.AddListener(HightlightAButton);
         }
     }
-     
+
+    
     public void BringNextQ()
     {
         DestroyImmediate(findQ());
         AButton.transform.GetChild(1).GetChild(0).GetComponent<MeshOutline>().enabled = false;
 
-        Qnow = (Qnow + 1)%Qs.Length;
+        Qnow = (Qnow + 1) % Qs.Length;
         GameObject NextQ = Managers.Resource.Instantiate(Qs[Qnow], transform);
         NextQ.transform.position = QPos;
 
         Questioning();
     }
 
+  
+
     GameObject findQ()
     {
-        int loopNum = 0;
         GameObject targetQ = null;
         foreach (Transform child in transform)
-        { 
+        {
             if (child.GetComponent<BoxCollider>() != null)
                 targetQ = child.gameObject;
-            if (loopNum++ > 10000)
-                throw new Exception("Infinite Loop");
-
         }
          return targetQ;
     }
 
-
+   
 }
