c4719d683e29fa6036269144f0af3878d614aeef
dhtnekfzz <dhtnekfzz@gnu.ac.kr>
2023-05-24 12:26:37 +0900

    Grocery_Shopping 최종본, 에셋 교체, Quest 중복 제거
    Former-commit-id: e21c335adb7ea8624a5d4f04d31efa0080b21ec2

--- Holo_lens/Assets/Resources/Scripts/Grocery_Shopping/Grocery_Shopping.cs
+++ Holo_lens/Assets/Resources/Scripts/Grocery_Shopping/Grocery_Shopping.cs
@@ -9,9 +9,11 @@ public class Grocery_Shopping : MonoBehaviour
 {
     private static Grocery_Shopping instance;
     //가격
-    public int tomatoPrice = 1000;
-    public int applePrice = 500;
-    public int pizzaPrice = 8500;
+    public int watermelonPrice = 15000;
+    public int applePrice = 1100;
+    public int cokePrice = 1200;
+    public int beerPrice = 2700;
+    public int bananaPrice = 900;
     public int totalPrice = 0;
     public TextMeshPro totalPriceText;
     public TextMeshPro question;
@@ -30,7 +32,7 @@ public class Grocery_Shopping : MonoBehaviour
 
     private void Start()
     {
-        Question();
+        Quest();
     }
 
 
@@ -40,10 +42,16 @@ public class Grocery_Shopping : MonoBehaviour
     {
         if (ob.CompareTag("Apple"))
             Add(ob);
-        else if (ob.CompareTag("Tomato"))
+        else if(ob.CompareTag("Watermelon"))
             Add(ob);
-        else if (ob.CompareTag("Pizza"))
+        else if (ob.CompareTag("Coke"))
             Add(ob);
+        else if (ob.CompareTag("Beer"))
+            Add(ob);
+        else if (ob.CompareTag("Banana"))
+            Add(ob);
+        
+        
     }
 
     // 계산대에서 물건이 빠지면 가격을 뺀다.
@@ -51,10 +59,15 @@ public class Grocery_Shopping : MonoBehaviour
     {
         if (ob.CompareTag("Apple"))
             Substract(ob);
-        else if (ob.CompareTag("Tomato"))
+        else if (ob.CompareTag("Watermelon"))
             Substract(ob);
-        else if (ob.CompareTag("Pizza"))
+        else if (ob.CompareTag("Coke"))
             Substract(ob);
+        else if (ob.CompareTag("Beer"))
+            Substract(ob);
+        else if (ob.CompareTag("Banana"))
+            Substract(ob);
+        
     }
 
     // 가격을 더하고 전체가격을 출력한다.
@@ -62,10 +75,17 @@ public class Grocery_Shopping : MonoBehaviour
     {
         if (ob.CompareTag("Apple"))
             totalPrice += applePrice;
-        else if (ob.CompareTag("Tomato"))
-            totalPrice += tomatoPrice;
-        else if (ob.CompareTag("Pizza"))
-            totalPrice += pizzaPrice;
+        else if (ob.CompareTag("Watermelon"))
+            totalPrice += watermelonPrice;
+        else if (ob.CompareTag("Coke"))
+            totalPrice += cokePrice;
+        else if (ob.CompareTag("Beer"))
+            totalPrice += beerPrice;
+        else if (ob.CompareTag("Banana"))
+            totalPrice += bananaPrice;
+
+
+        
         UpdateTotalPrice();
     }
 
@@ -74,10 +94,15 @@ public class Grocery_Shopping : MonoBehaviour
     {
         if (ob.CompareTag("Apple"))
             totalPrice -= applePrice;
-        else if (ob.CompareTag("Tomato"))
-            totalPrice -= tomatoPrice;
-        else if (ob.CompareTag("Pizza"))
-            totalPrice -= pizzaPrice;
+        else if (ob.CompareTag("Watermelon"))
+            totalPrice -= watermelonPrice;
+        else if (ob.CompareTag("Coke"))
+            totalPrice -= cokePrice;
+        else if (ob.CompareTag("Beer"))
+            totalPrice -= beerPrice;
+        else if (ob.CompareTag("Banana"))
+            totalPrice -= bananaPrice;
+        
         UpdateTotalPrice();
     }
 
@@ -89,24 +114,35 @@ public class Grocery_Shopping : MonoBehaviour
     //구매해야할 물품을 랜덤으로 알려준다.
     private void Quest()
     {
-        string[] ob = { "사과", "바나나", "오렌지", "포도", "수박", "참외" };
+        string[] ob = { "수박", "바나나", "사과", "콜라", "맥주" };
         int minCount = 1;
-        int maxCount = 5;
+        int maxCount = 2;
 
-        int obCount = UnityEngine.Random.Range(1, 4);
+        int obCount = UnityEngine.Random.Range(1, 5);
         string[] selectedob = new string[obCount];
 
         for (int i = 0; i < obCount; i++)
         {
             int index = UnityEngine.Random.Range(0, ob.Length);
+
+            //중복 제거
+            for (int j = 0; j < i; j++)
+            {
+                if (selectedob[j] == ob[index])
+                {
+                    index = UnityEngine.Random.Range(0, ob.Length);
+                    j = -1;
+                }
+            }
             selectedob[i] = ob[index];
+
         }
 
         string[] obstring = new string[obCount];
         for (int i = 0; i < obCount; i++)
         {
             int count = UnityEngine.Random.Range(minCount, maxCount + 1);
-            obstring[i] = string.Format("{0}{1}개", selectedob[i], count);
+            obstring[i] = string.Format("{0} {1}개", selectedob[i], count);
         }
 
         string result = string.Join(",", obstring);
