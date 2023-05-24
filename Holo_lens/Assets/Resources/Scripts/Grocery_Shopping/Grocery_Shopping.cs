f73a091a1b6557ef8e00e890becd2f19ff6b64e6
dhtnekfzz <dhtnekfzz@gnu.ac.kr>
2023-05-24 19:25:46 +0900

    Grocery_Shopping(aseet교체, 오류 수정)최종
    commit ad7e7300a3ffeb9ecd17ac6c156188a7c365f465
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 19:12:44 2023 +0900
    
        bfg
    
    commit 8ed076300bdc6ff7e5dff17874be496759bb1d3f
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 16:43:14 2023 +0900
    
        셋팅
    
        Former-commit-id: 3a4576464cafd38f1875e74ea2223cc7b3c36c20
    
    commit b4ed9d2644f02347770ff42d998f34908bc64dd5
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 14:31:09 2023 +0900
    
        파일 용량 수정
    
        Former-commit-id: cb8add2171f476b9e8917ac7bfa105160ac8122e
    
    commit d8241e87c24a7cc006a5637904831c865bba00f0
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 14:20:40 2023 +0900
    
        Grocery_Shopping 최종본, 에셋 교체, Quest 중복 제거
    
        Former-commit-id: d8203e697bed1015cd2b090be0a78b058c2f136b
    
    commit a3350719aaf64f1961b1c97320ab90cefce43b39
    Merge: 0c77214 0f98eaa
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 14:10:01 2023 +0900
    
        Merge
    
        Former-commit-id: f78e12f4ea97da28400707b086e12e9783b75fc6
    
    commit 0c7721487c5c13b81f7f0b4f408151289c9a3d83
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 12:45:22 2023 +0900
    
        Grocery_Shopping 최종본, 에셋 교체, Quest 중복 제거
    
        Former-commit-id: d749009e5b233d5bd254d518b3b6159a468b52ac
    
    commit 918cb6620e85bcfade142a1d1232b9e49d3e9073
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 12:27:46 2023 +0900
    
        Grocery_Shopping 최종본, 에셋 교체, Quest 중복 제거
    
        Former-commit-id: 20530f22f82dbaaf3cd57cf11a5f9ab1ba682ad7
    
    commit b8746d2f79420fc5d777a5839c651970ba1b9259
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 12:27:09 2023 +0900
    
        Grocery_Shopping 최종본, 에셋 교체, Quest 중복 제거
    
        Former-commit-id: 45ce3b8a52809f17c95d18e853d4713a726c36c2
    
    commit c4719d683e29fa6036269144f0af3878d614aeef
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Wed May 24 12:26:37 2023 +0900
    
        Grocery_Shopping 최종본, 에셋 교체, Quest 중복 제거
    
        Former-commit-id: e21c335adb7ea8624a5d4f04d31efa0080b21ec2
    
    commit 8ad086038a9d7368d9958df564e55168fdb166b5
    Author: dhtnekfzz <dhtnekfzz@gnu.ac.kr>
    Date:   Mon May 22 23:59:44 2023 +0900
    
        Grocery_Shopping (PayMoney거스름 돈 단위 별 계산, Quest )

--- Holo_lens/Assets/Resources/Scripts/Grocery_Shopping/Grocery_Shopping.cs
+++ Holo_lens/Assets/Resources/Scripts/Grocery_Shopping/Grocery_Shopping.cs
@@ -1,4 +1,5 @@
 using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities.FigmaImporter;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,8 +9,17 @@ using UnityEngine;
 public class Grocery_Shopping : MonoBehaviour
 {
     private static Grocery_Shopping instance;
-
+    //가격
+    public int watermelonPrice = 15000;
+    public int applePrice = 1100;
+    public int cokePrice = 1200;
+    public int beerPrice = 2700;
+    public int bananaPrice = 900;
+    public int totalPrice = 0;
+    public TextMeshPro totalPriceText;
+    public TextMeshPro question;
     private Grocery_Shopping() { }
+   
     public static Grocery_Shopping Instance
     {
         get
@@ -22,22 +32,30 @@ public class Grocery_Shopping : MonoBehaviour
         }
     }
 
-    //가격
-    public int tomatoPrice = 1000;
-    public int applePrice = 500;
-    public int pizzaPrice = 8500;
-    public int totalPrice = 0;
-    public TextMeshPro totalPriceText;
+
+    private void Start()
+    {
+        Quest();
+    }
+
+
+
 
     // 계산대에 물건이 들어오면 가격을 더한다.
     private void OnTriggerEnter(Collider ob)
     {
         if (ob.CompareTag("Apple"))
             Add(ob);
-        else if (ob.CompareTag("Tomato"))
+        else if(ob.CompareTag("Watermelon"))
             Add(ob);
-        else if (ob.CompareTag("Pizza"))
+        else if (ob.CompareTag("Coke"))
             Add(ob);
+        else if (ob.CompareTag("Beer"))
+            Add(ob);
+        else if (ob.CompareTag("Banana"))
+            Add(ob);
+        
+        
     }
 
     // 계산대에서 물건이 빠지면 가격을 뺀다.
@@ -45,10 +63,15 @@ public class Grocery_Shopping : MonoBehaviour
     {
         if (ob.CompareTag("Apple"))
             Substract(ob);
-        else if (ob.CompareTag("Tomato"))
+        else if (ob.CompareTag("Watermelon"))
             Substract(ob);
-        else if (ob.CompareTag("Pizza"))
+        else if (ob.CompareTag("Coke"))
             Substract(ob);
+        else if (ob.CompareTag("Beer"))
+            Substract(ob);
+        else if (ob.CompareTag("Banana"))
+            Substract(ob);
+        
     }
 
     // 가격을 더하고 전체가격을 출력한다.
@@ -56,10 +79,17 @@ public class Grocery_Shopping : MonoBehaviour
     {
         if (ob.CompareTag("Apple"))
             totalPrice += applePrice;
-        else if (ob.CompareTag("Tomato"))
-            totalPrice += tomatoPrice;
-        else if (ob.CompareTag("Pizza"))
-            totalPrice += pizzaPrice;
+        else if (ob.CompareTag("Watermelon"))
+            totalPrice += watermelonPrice;
+        else if (ob.CompareTag("Coke"))
+            totalPrice += cokePrice;
+        else if (ob.CompareTag("Beer"))
+            totalPrice += beerPrice;
+        else if (ob.CompareTag("Banana"))
+            totalPrice += bananaPrice;
+
+
+        
         UpdateTotalPrice();
     }
 
@@ -68,10 +98,15 @@ public class Grocery_Shopping : MonoBehaviour
     {
         if (ob.CompareTag("Apple"))
             totalPrice -= applePrice;
-        else if (ob.CompareTag("Tomato"))
-            totalPrice -= tomatoPrice;
-        else if (ob.CompareTag("Pizza"))
-            totalPrice -= pizzaPrice;
+        else if (ob.CompareTag("Watermelon"))
+            totalPrice -= watermelonPrice;
+        else if (ob.CompareTag("Coke"))
+            totalPrice -= cokePrice;
+        else if (ob.CompareTag("Beer"))
+            totalPrice -= beerPrice;
+        else if (ob.CompareTag("Banana"))
+            totalPrice -= bananaPrice;
+        
         UpdateTotalPrice();
     }
 
@@ -80,5 +115,59 @@ public class Grocery_Shopping : MonoBehaviour
     {
         totalPriceText.text = "가격:" + totalPrice.ToString("N0") + "원";
     }
+    //구매해야할 물품을 랜덤으로 알려준다.
+    private void Quest()
+    {
+        string[] ob = { "수박", "바나나", "사과", "콜라", "맥주" };
+        int minCount = 1;
+        int maxCount = 2;
 
+        int obCount = UnityEngine.Random.Range(1, 5);
+        string[] selectedob = new string[obCount];
+
+        for (int i = 0; i < obCount; i++)
+        {
+            int index = UnityEngine.Random.Range(0, ob.Length);
+
+            //중복 제거
+            for (int j = 0; j < i; j++)
+            {
+                if (selectedob[j] == ob[index])
+                {
+                    index = UnityEngine.Random.Range(0, ob.Length);
+                    j = -1;
+                }
+            }
+            selectedob[i] = ob[index];
+
+        }
+
+        string[] obstring = new string[obCount];
+        for (int i = 0; i < obCount; i++)
+        {
+            int count = UnityEngine.Random.Range(minCount, maxCount + 1);
+            obstring[i] = string.Format("{0} {1}개", selectedob[i], count);
+        }
+
+        string result = string.Join(",", obstring);
+        question.text = string.Format("{0}를 구매하세요", result);
+
+    
+    }
+
+    private void Question()
+    {
+         string[] fruits = { "사과", "바나나", "오렌지", "포도", "수박", "참외" };
+         int minCount = 1;
+         int maxCount = 5;
+
+        
+        int[] randomIndex = new int[3];string fruit1 = fruits[UnityEngine.Random.Range(0, fruits.Length)];
+        string fruit2 = fruits[UnityEngine.Random.Range(0, fruits.Length)];
+        int count1 = UnityEngine.Random.Range(minCount, maxCount + 1);
+        int count2 = UnityEngine.Random.Range(minCount, maxCount + 1);
+
+        question.text =string.Format("{0}{1}개 {2}{3}개를 사세요", fruit1, count1, fruit2, count2);
+        
+    }
 }
